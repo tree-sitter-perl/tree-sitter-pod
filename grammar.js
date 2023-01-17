356d3421134aa7942eff1da1ed2758a6cca1146c
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2023-01-17 19:33:57 +0000

    Handle =over/=item/=back sequences

--- grammar.js
+++ grammar.js
@@ -9,6 +9,7 @@ module.exports = grammar({
   rules: {
     pod: $ => repeat(choice(
       $.head_paragraph,
+      $.indented_block,
       $.plain_paragraph,
       $.verbatim_paragraph,
 
@@ -19,6 +20,19 @@ module.exports = grammar({
 
     head_paragraph: $ => seq($._start_directive, $.head_directive, $.content, $._eol),
     head_directive: $ => choice('=head1', '=head2', '=head3', '=head4'),
+
+    indented_block: $ => seq(
+      $.over_paragraph,
+      repeat(choice($.item_paragraph, $.plain_paragraph, $.verbatim_paragraph, $._blank_line)),
+      $.back_paragraph,
+    ),
+
+    over_paragraph: $ => seq($._start_directive, $.over_directive, $.content, $._eol),
+    over_directive: $ => '=over',
+    item_paragraph: $ => seq($._start_directive, $.item_directive, $.content, $._eol),
+    item_directive: $ => '=item',
+    back_paragraph: $ => seq($._start_directive, $.back_directive, $._eol),
+    back_directive: $ => '=back',
 
     plain_paragraph: $ => seq($._start_plain, repeat(seq($.content, $._eol)), $._eol),
 
