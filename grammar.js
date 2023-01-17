8fc68dd5b8ed9ce76a793471d303387d92c2a1d8
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2023-01-17 18:22:20 +0000

    Recognise =head[N] paragraphs

--- grammar.js
+++ grammar.js
@@ -1,6 +1,13 @@
 module.exports = grammar({
   name: 'pod',
   rules: {
-    pod: $ => seq(),
+    pod: $ => repeat(choice(
+      $.head_paragraph,
+    )),
+
+    head_paragraph: $ => seq($.head_directive, $.content),
+    head_directive: $ => choice('=head1', '=head2', '=head3', '=head4'),
+
+    content: $ => /.+/,
   },
 })
