ea5d557cbd185cdcb5efcfdb6bc846fe909d86ae
GH Actions [beep-boop] <release-flow@beep.boop>
2023-10-16 18:01:08 +0000

    deploy: c740cc5d9912711c648040217b418a8c09330f23

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -33,34 +33,39 @@
 
 (interior_sequence
   (sequence_letter) @character
-  "<" @punctuation.delimiter
-  ">" @punctuation.delimiter
+  ["<" ">"] @punctuation.delimiter
 )
 
 (interior_sequence
   (sequence_letter) @character
   (#eq? @character "B")
   (content) @text.strong)
+
 (interior_sequence
   (sequence_letter) @character
   (#eq? @character "C")
   (content) @text.literal)
+
 (interior_sequence
   (sequence_letter) @character
   (#eq? @character "F")
   (content) @text.underline @string.special)
+
 (interior_sequence
   (sequence_letter) @character
   (#eq? @character "I")
   (content) @text.emphasis)
+
 (interior_sequence
   (sequence_letter) @character
   (#eq? @character "L")
   (content) @text.uri)
+
 (interior_sequence
   (sequence_letter) @character
   (#eq? @character "X")
   (content) @text.reference)
+
 (interior_sequence
   (sequence_letter) @character
   (#eq? @character "E")
