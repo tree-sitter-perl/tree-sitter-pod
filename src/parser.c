3f15a3f11b422753fbf985190dceacb4bbf80ecf
GH Actions [beep-boop] <release-flow@beep.boop>
2026-03-23 15:19:07 +0000

    deploy: 57c606aa3373ba876d44113d13fe7bdc2c060723

--- src/parser.c
+++ src/parser.c
@@ -1255,9 +1255,9 @@ TS_PUBLIC const TSLanguage *tree_sitter_pod(void) {
     .name = "pod",
     .max_reserved_word_set_size = 0,
     .metadata = {
-      .major_version = 0,
+      .major_version = 1,
       .minor_version = 0,
-      .patch_version = 1,
+      .patch_version = 0,
     },
   };
   return &language;
